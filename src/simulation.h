7df9b21d4d9d117bf4cd8e4f4fbf1cbd1e33bcf0
Rene Lohmann <r.lohmann@ed.ac.uk>
2025-03-09 00:18:37 +0100

    made noise space dependent

--- src/simulation.h
+++ src/simulation.h
@@ -472,7 +472,7 @@ constexpr double wx_harmonic = 1;
 constexpr double wy_harmonic = 100;
 inline void Simulation:: compute_force_Harmonic_Noisy(){
 
-    params.force.x = -2*wx_harmonic*params.position.x + sigma_harmonic*normal(twister);
-    params.force.y = -2*wy_harmonic*params.position.y + sigma_harmonic*normal(twister);
+    params.force.x = -2*wx_harmonic*params.position.x;
+    params.force.y = -2*wy_harmonic*params.position.y + params.position.y*sigma_harmonic*normal(twister);
 
 }
