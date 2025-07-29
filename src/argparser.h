64b5ee3c4ce0515eb63c8502e53246d426200938
Rene Lohmann <r.lohmann@ed.ac.uk>
2025-07-29 23:23:40 +0100

    update readme

--- src/argparser.h
+++ src/argparser.h
@@ -55,7 +55,7 @@ std:: pair <cxxopts:: ParseResult, cxxopts:: Options> parseCommandLine(int argc,
         ("sundman_M",       "Hyperparameter M (only used by ZBAOABZ)",                  cxxopts:: value <double>()->default_value(_Sundman_M_default))           
         ("forcefield",      "Forcefield. Allowed values are 'muellerbrown', 'ackley'," 
                             "'rosenbrock', 'beale', 'entropicbarrier'," 
-                             "'star', 'neal', or 'harmonic_asym'.",                     cxxopts:: value <std:: string>()->default_value(_forcefield_default))         
+                            "'star', 'neal', or 'harmonic_asym'.",                     cxxopts:: value <std:: string>()->default_value(_forcefield_default))         
         ("init_position",   "Initial position. Enter two comma-separated floats.",      cxxopts:: value<std::vector<double>>()->default_value(_init_position_default))
         ("init_velocity",   "Initial velocity. Enter two comma-separated floats.",      cxxopts:: value<std::vector<double>>()->default_value(_init_velocity_default))
         ("N_iteration",     "Number of simulation steps.",                              cxxopts:: value <int>()->default_value(_N_iteration_default))
